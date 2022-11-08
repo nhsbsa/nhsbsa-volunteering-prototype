5a4b119eea624bf731f88746d6008559488a66d5
“NaomiGrant2022” <naomi.grant4@nhs.net>
2022-11-08 16:20:54 +0000

    Sprint 6, Recruiter journey 2

--- app/routes.js
+++ app/routes.js
@@ -2,24 +2,19 @@
 const express = require('express');
 
 const router = express.Router();
-// Run this code when a form is submitted to 'reuse-data-answer'
-router.post('/reuse-data-answer', function (req, res) {
 
-    // Make a variable and give it the value from 'reuse-data'
-    var reuseData = req.session.data['reuse-data']
+const radioButtonRedirect = require('radio-button-redirect');
+router.use(radioButtonRedirect)
+
   
-    // Check whether the variable matches a condition
-    if (reuseData == "yes"){
-      // Send user to next page
-      res.redirect('/sprint-2/sign-in')
-    } else {
-      // Send user to ineligible page
-      res.redirect('/sprint-2/ineligible')
-    }
-  
-  })
 // Add your routes here - above the module.exports line
 
+
+
 module.exports = router;
+
+
+
+
 
 
